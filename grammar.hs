dcd2443798bfb0f08e4ba798d291058c5c5e3abf
Marcelo <marcelopjmuniz@gmail.com>
2018-09-13 15:03:28 -0300

    More cases covered on eval

--- grammar.hs
+++ grammar.hs
@@ -14,24 +14,32 @@ data Bexpr = Boo Bool | Eq Expr Expr | Not Bexpr deriving Show
 data Statement = Exp Expr | Command Cmd deriving Show
 data Cmd = Assign Identifier Expr | Loop Bexpr Cmd | CSeq Cmd Cmd deriving Show
 
+data Value = Bo Bool | In Int deriving Show
 {- |
  - Automata definition
  -}
-type ValueStack = [String]
-type ControlStack = [String]
+type ValueStack = [Value]
+type ControlStack = [Expr]
 data ExpPiAut = ExpPiAut { val :: ValueStack,
                            cnt :: ControlStack
                          } deriving Show
 {- |
  - automata functions evaluation
 -}
+{-
 evalSum :: ExpPiAut -> Aexpr -> Aexpr -> ExpPiAut
 evalSum (ExpPiAut v c) aex1 aex2 = ExpPiAut {cnt = ("SUM" : show aex2 : show aex1 : c ), val = v}
-
+-}
 -- TODO: implement eval
 eval :: ExpPiAut -> ExpPiAut
-eval (ExpPiAut v c) = if typeOf (head c) true (evalSum ExpPiAut {cnt = rest} (head ) (head v)
-
+eval (ExpPiAut v c) = case (head c) of
+                           Aexp (Sum aex1 aex2) -> eval ExpPiAut {cnt = Aexp aex1 : Aexp aex2 : c, val = v} -- TODO: discover if KWs are needed here, and why
+                           Aexp (Sub aex1 aex2) -> eval ExpPiAut {cnt = Aexp aex1 : Aexp aex2 : c, val = v}
+                           Aexp (Mul aex1 aex2) -> eval ExpPiAut {cnt = Aexp aex1 : Aexp aex2 : c, val = v}
+                           Bexp (Eq  exp1 exp2) -> eval ExpPiAut {cnt = Bexp exp1 : Bexp exp2 : c, val = v} -- TODO: match types
+                           Aexp (Num ival)      -> eval ExpPiAut {cnt = tail c, val = In ival : v}
+                           Bexp (Boo bval)      -> eval ExpPiAut {cnt = tail c, val = Bo bval : v}
+                           Bexp (Not ex)        -> eval ExpPiAut {cnt = Bexp ex : c, val = v}
 {- 
 data ExpKW = SUM | SUB | MUL | EQ | NOT
 evalSum aut exp exp = aut&cnt 
