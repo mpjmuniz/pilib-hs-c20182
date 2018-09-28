78c77ac41f35fa67e4f93b402e9e5b3200bb494b
Marcelo Muniz <marcelopjm@id.uff.br>
2018-09-28 10:36:06 -0300

    inifite loops and zero-passes loops now working
    missing test cases for 1+ terminal loop cases.

--- Calc.hs
+++ Calc.hs
@@ -6,7 +6,58 @@ import Data.Maybe
 import System.IO
 import qualified Data.Map.Strict as Map
 
+{- FOR USE ON REPL.IT
+data Expr = Aexp Aexpr 
+        | Bexp Bexpr 
+        | Idtf Identifier 
+        | Kw Keyword
+        | Comm Cmd deriving (Show, Eq)
+		 
+data Aexpr = Num Int 
+        | Sum Aexpr Aexpr 
+        | Sub Aexpr Aexpr 
+        | Mul Aexpr Aexpr deriving (Show, Eq)
 
+data Bexpr = Boo Bool 
+        | Eq Bexpr Bexpr 
+        | Not Bexpr 
+		| Gt Aexpr Aexpr 
+		| Ge Aexpr Aexpr 
+		| Lt Aexpr Aexpr 
+		| Le Aexpr Aexpr
+		| And Bexpr Bexpr 
+		| Or Bexpr Bexpr deriving (Show, Eq)
+
+
+data Statement = Exp Expr 
+        | Command Cmd deriving Show
+
+data Cmd = Assign Identifier Expr 
+        | Loop Bexpr Cmd 
+        | CSeq Cmd Cmd deriving (Show, Eq)
+
+data Keyword = KWSum 
+        | KWMul 
+        | KWSub 
+        | KWEq 
+        | KWNot
+        | KWOr 
+        | KWAnd
+        | KWLt 
+        | KWLe
+        | KWGt 
+        | KWGe
+        | KWAssign 
+        | KWLoop deriving (Show, Eq)
+
+data Identifier = Id String deriving (Show, Eq, Ord)
+
+data Value = Bo { bval :: Bool } 
+        | In { ival :: Int } 
+        | Idt { idval :: Identifier } 
+        | Lp {beval :: Bexpr, cmdval :: Cmd} 
+        | Comd {cval :: Cmd } deriving (Show, Eq)
+-}
 {- |
  - Automata definition
  -}
@@ -35,11 +86,11 @@ lookup' k m = fromJust $ Map.lookup k m
 eval :: CmdPiAut -> CmdPiAut
 eval cpa@(CmdPiAut {cnt = []}) = cpa
 eval cpa@(CmdPiAut e s v c)    = eval $ case (head c) of
-                                         Comm (Assign idtf exp) -> cpa{cnt = exp : Kw KWAssign : tail c, val = Idt idtf : v, env = Map.insert idtf (L $ Map.size e + 1) e} -- TODO: check for "S' = S/[I->T]"
+                                         Comm (Assign idtf exp) -> cpa{cnt = exp : Kw KWAssign : tail c, val = Idt idtf : v, env = Map.insert idtf (L $ Map.size e + 1) e} 
                                          Comm (CSeq cmd1 cmd2)  -> cpa{cnt = Comm cmd1 : Comm cmd2 : tail c}
-                                         Comm (Loop bexp cmd)   -> cpa{cnt = Bexp bexp : Kw KWLoop : tail c, val = Comd cmd : Lp bexp cmd : v}
+                                         Comm (Loop bexp cmd)   -> cpa{cnt = Bexp bexp : Kw KWLoop : tail c, val = Lp bexp cmd : v}
                                          Aexp (Sum aex1 aex2)   -> cpa{cnt = Aexp aex1 : Aexp aex2 : Kw KWSum : tail c} 
-                                         Aexp (Sub aex1 aex2)   -> cpa{cnt = Aexp aex2: Aexp aex1: Kw KWSub : tail c}
+                                         Aexp (Sub aex1 aex2)   -> cpa{cnt = Aexp aex2 : Aexp aex1 : Kw KWSub : tail c}
                                          Aexp (Mul aex1 aex2)   -> cpa{cnt = Aexp aex1 : Aexp aex2 : Kw KWMul : tail c}
                                          Bexp (Eq  exp1 exp2)   -> cpa{cnt = Bexp exp1 : Bexp exp2 : Kw KWEq  : tail c} 
                                          Bexp (Or  exp1 exp2)   -> cpa{cnt = Bexp exp1 : Bexp exp2 : Kw KWOr  : tail c} 
@@ -55,10 +106,10 @@ eval cpa@(CmdPiAut e s v c)    = eval $ case (head c) of
                                          Kw KWNot               -> cpa{cnt = tail c, val = Bo (not (bval (head v))) : tail v}
                                          x -> let (va:vb:vs) = v in
                                                             case x of 
-                                                            Kw KWAssign -> cpa{cnt = tail c, val = vs, sto = Map.insert (lookup' (idval vb) e) va s } -- This lookup can't find anything because it has not been there yet!
+                                                            Kw KWAssign -> cpa{cnt = tail c, val = vs, sto = Map.insert (lookup' (idval vb) e) va s }
                                                             Kw KWLoop -> cpa{cnt = if(bval $ va) then (Comm $ Loop (beval vb) (cmdval vb)) : tail c 
                                                                                                  else tail c
-                                                                            , val = vs}
+                                                                            ,val = vs}
                                                             Kw KWSum -> cpa{cnt = tail c, val = In (ival va + ival vb) : vs}
                                                             Kw KWSub -> cpa{cnt = tail c, val = In (ival va - ival vb) : vs}
                                                             Kw KWMul -> cpa{cnt = tail c, val = In (ival va * ival vb) : vs}
@@ -76,7 +127,8 @@ main = do
     --let ast = parseCalc (scanTokens s)
     --let ast = Comm (Assign (Id "Meu ID") (Aexp $ Sum (Num 1) (Num 1)))
     --let ast = Comm (CSeq (Assign (Id "Meu ID") (Aexp $ Sum (Num 1) (Num 1))) (Assign (Id "Meu ID Denovo!") (Aexp $ Sum (Num 1) (Num 1))))
-    let ast = Comm (Loop (Eq (Boo False) (Boo False)) (Assign (Id "Meu ID") (Aexp $ Sum (Num 1) (Num 1))))
+    --let ast = Comm (Loop (Eq (Boo True) (Boo False)) (Assign (Id "Meu ID") (Aexp $ Sum (Num 1) (Num 1))))
+    let ast = Comm (CSeq (Assign (Id "Meu ID") (Aexp $ Num 1)) (Loop (Lt (Id "Meu ID") (Num 5)) (Assign (Id "Meu ID") (Aexp $ Sum (Id "Meu ID") (Num 1)))))
     let aut = CmdPiAut (Map.fromList []) (Map.fromList []) [] [ast]
     let result =  eval aut
     print ast
@@ -96,7 +148,7 @@ Lt (Num 1) (Num 2)
 Le (Num 2) (Num 2)
 And (Boo True) (Boo True)
 Or (Boo True) (Boo False)
--- até aqui, tudo OK
+-- atÃ© aqui, tudo OK
 Id "Meu ID"
 Idtf (Id "Meu ID")
 Assign (Id "Meu ID") (Sum (Num 1) (Num 1))
@@ -107,7 +159,5 @@ Exp (Aexp (Sum (Num 1) (Num 1)))
 Command (Assign (Id "Meu ID") (Sum (Num 1) (Num 1)))
 Loop (Eq (Boo True) (Boo False)) (Assign (Id "Meu ID") (Sum (Num 1) (Num 1)))
 CSeq (Assign (Id "Meu ID") (Sum (Num 1) (Num 1))) (Assign (Id "Meu ID denovo") (Sum (Num 1) (Num 1))
-
 let exp = Sum (Num 5) (Num 2)
-
 -}
