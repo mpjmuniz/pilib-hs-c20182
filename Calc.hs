25d989d9581c4101ca00e1a221d7687f7d423282
rodrigomesteves <rodrigomarins@id.uff.br>
2018-09-28 00:24:59 -0300

    Updated project

---
+++ Calc.hs
@@ -0,0 +1,81 @@
+module Main where
+import Parser
+import Lexer
+import Data.Dynamic
+import Data.Maybe
+import System.IO
+import qualified Data.Map.Strict as Map
+
+
+{- |
+ - Automata definition
+ -}
+ 
+data Location = L Integer 
+        | Sto Storable deriving (Show, Eq, Ord) 
+
+data Storable = N Int 
+        | B Bool deriving (Show, Eq, Ord)
+
+		
+type ValueStack = [Value]
+type ControlStack = [Expr]
+type Loc = Integer
+
+--[(Value,Expr)] identifier -> location
+--[(Expr,Value)] location -> value
+data CmdPiAut = CmdPiAut { env :: Map.Map Identifier Location,
+                           sto :: Map.Map Location Value, -- TODO: mudar para Storable
+                           val :: ValueStack,
+                           cnt :: ControlStack
+                         } deriving Show
+
+lookup' :: Ord k => k -> Map.Map k a -> a
+lookup' k m = fromJust $ Map.lookup k m
+
+eval :: CmdPiAut -> CmdPiAut
+eval cpa@(CmdPiAut {cnt = []}) = cpa
+eval cpa@(CmdPiAut e s v c)    = eval $ case (head c) of
+                                         Comm (Assign idtf exp) -> cpa{cnt = exp : Kw KWAssign : tail c, val = Idt idtf : v} -- TODO: check for "S' = S/[I->T]"
+                                         Comm (CSeq cmd1 cmd2)  -> cpa{cnt = Comm cmd1 : Comm cmd2 : tail c}
+                                         Comm (Loop bexp cmd)   -> cpa{cnt = Bexp bexp : Kw KWLoop : tail c, val = Comd cmd : Lp bexp cmd : v}
+                                         Aexp (Sum aex1 aex2)   -> cpa{cnt = Aexp aex1 : Aexp aex2 : Kw KWSum : tail c} 
+                                         Aexp (Sub aex1 aex2)   -> cpa{cnt = Aexp aex2: Aexp aex1: Kw KWSub : tail c}
+                                         Aexp (Mul aex1 aex2)   -> cpa{cnt = Aexp aex1 : Aexp aex2 : Kw KWMul : tail c}
+                                         Bexp (Eq  exp1 exp2)   -> cpa{cnt = Bexp exp1 : Bexp exp2 : Kw KWEq  : tail c} 
+                                         Bexp (Or  exp1 exp2)   -> cpa{cnt = Bexp exp1 : Bexp exp2 : Kw KWOr  : tail c} 
+                                         Bexp (And exp1 exp2)   -> cpa{cnt = Bexp exp1 : Bexp exp2 : Kw KWAnd : tail c} 
+                                         Bexp (Lt  exp1 exp2)   -> cpa{cnt = Aexp exp2: Aexp exp1: Kw KWLt  : tail c} 
+                                         Bexp (Le  exp1 exp2)   -> cpa{cnt = Aexp exp2: Aexp exp1: Kw KWLe  : tail c} 
+                                         Bexp (Ge  exp1 exp2)   -> cpa{cnt = Aexp exp2: Aexp exp1: Kw KWGe  : tail c} 
+                                         Bexp (Gt  exp1 exp2)   -> cpa{cnt = Aexp exp2: Aexp exp1: Kw KWGt  : tail c} 
+                                         Bexp (Not ex)          -> cpa{cnt = Bexp ex : Kw KWNot : tail c}
+                                         Aexp (Num ival)        -> cpa{cnt = tail c, val = In ival : v}
+                                         Bexp (Boo bval)        -> cpa{cnt = tail c, val = Bo bval : v}
+                                         Idtf (Id str)          -> cpa{cnt = tail c, val = lookup' (lookup' (Id str) e) s : v}
+                                         Kw KWNot               -> cpa{cnt = tail c, val = Bo (not (bval (head v))) : tail v}
+                                         x -> let (va:vb:vs) = v in
+                                                            case x of 
+                                                            Kw KWAssign -> cpa{cnt = tail c, val = vs, sto = Map.insert (lookup' (idval vb) e) va s }
+                                                            Kw KWLoop -> cpa{cnt = if(bval $ va) then (Comm $ Loop (beval vb) (cmdval vb)) : tail c 
+                                                                                                 else tail c
+                                                                            , val = vs}
+                                                            Kw KWSum -> cpa{cnt = tail c, val = In (ival va + ival vb) : vs}
+                                                            Kw KWSub -> cpa{cnt = tail c, val = In (ival va - ival vb) : vs}
+                                                            Kw KWMul -> cpa{cnt = tail c, val = In (ival va * ival vb) : vs}
+                                                            Kw KWEq  -> cpa{cnt = tail c, val = Bo (bval va == bval vb) : vs}
+                                                            Kw KWLe  -> cpa{cnt = tail c, val = Bo (ival va >= ival vb) : vs}
+                                                            Kw KWLt  -> cpa{cnt = tail c, val = Bo (ival va > ival vb) : vs}
+                                                            Kw KWGe  -> cpa{cnt = tail c, val = Bo (ival va <= ival vb) : vs}
+                                                            Kw KWGt  -> cpa{cnt = tail c, val = Bo (ival va < ival vb) : vs}
+                                                            Kw KWOr  -> cpa{cnt = tail c, val = Bo (bval va || bval vb) : vs}
+                                                            Kw KWAnd -> cpa{cnt = tail c, val = Bo (bval va && bval vb) : vs}
+main :: IO ()
+main = do	
+    s <- readFile "C:\\Users\\rodri\\OneDrive\\Documentos\\compiladores\\program.txt"
+    print s
+    let ast = parseCalc (scanTokens s)
+    let aut = CmdPiAut (Map.fromList []) (Map.fromList []) [] [ast]
+    let result =  eval aut
+    print ast
+    print result
---
+++ Lexer.x
@@ -0,0 +1,78 @@
+{
+module Lexer where
+import Data.Bool
+}
+
+%wrapper "basic"
+
+$digit = 0-9			-- digits
+$alpha = [a-zA-Z]		-- alphabetic characters
+$whitechar = [\s\n\r]
+
+tokens :-
+  $white+			;
+  $digit+			{ \s -> TokenInt(read s)}	
+  ":="                           { \s -> TokenAssign}
+  \==                           { \s -> TokenEq }
+  \+                            { \s -> TokenPlus }
+  \-                            { \s -> TokenMinus }
+  \*                            { \s -> TokenTimes }
+  \/                            { \s -> TokenDiv }
+  \(                            { \s -> TokenLParen }
+  \)                            { \s -> TokenRParen }
+  \!                            { \s -> TokenNot }
+  \>                            { \s -> TokenMaior }
+  \<                            { \s -> TokenMenor }
+  \>=                           { \s -> TokenMaiorIgual }
+  \<=                           { \s -> TokenMenorIgual }
+  \&&							{ \s  -> TokenAnd}
+  "||"							{ \s  -> TokenOr}
+  while                       { \s  -> TokenWhile}
+  do                          { \s  -> TokenDo}
+  \{                           { \s  -> TokenLBrace}
+  \}                           { \s  -> TokenRBrace}
+  \;                           { \s   -> TokenSemiComma}
+  False			                { \s -> TokenFalse}
+  True					        { \s -> TokenTrue}
+  $alpha[$alpha $digit \_]*		{ \s -> TokenVarId s}
+
+
+{
+-- Each action has type :: String -> Token
+
+-- The token type:
+data Token = TokenVarId String
+           | TokenAssign
+           | TokenEq
+           | TokenPlus
+           | TokenMinus
+           | TokenTimes
+           | TokenDiv
+           | TokenLParen
+           | TokenRParen
+           | TokenNot 
+           | TokenInt Int
+           | TokenMaior
+           | TokenMenor
+           | TokenMaiorIgual
+           | TokenMenorIgual
+		   | TokenAnd
+		   | TokenOr
+		   | TokenWhile
+		   | TokenDo
+		   | TokenLBrace
+		   | TokenRBrace
+		   | TokenSemiComma
+		   | TokenTrue
+           | TokenFalse	deriving (Eq,Show)
+
+scanTokens = alexScanTokens
+
+{-
+main::IO ()
+main = do
+  s <- getContents
+  let x = alexScanTokens s
+  mapM_ print x
+-}
+} 
